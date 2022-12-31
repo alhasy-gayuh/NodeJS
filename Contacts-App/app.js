6eb3d7e73e3a7b339899754bc6cb2bdb31d90b77
alhasy-gayuh <alhasygayuh@gmail.com>
2023-01-01 00:51:11 +0700

    membuat contact app dengan commandline interface

--- Contacts-App/app.js
+++ Contacts-App/app.js
@@ -1,13 +1,30 @@
-const contacts = require('./contacts')
+// membuat contact app secara commandline interface 
+const yargs = require('yargs');
+const { saveContact } = require('./contacts');
 
-const main = async () => {
-    const nama = await  contacts.createQuest('Masukan nama anda : ');
-    const email = await contacts.createQuest('Masukan email anda : ');
-    const noHp = await  contacts.createQuest('Masukan noHp anda : ');
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+        nama: {
+            describe: 'Nama Lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Alamat Email',
+            demandOption: false,
+            type: 'string',
+        },
+        noHp: {
+            describe: 'No. Handphone',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv){
+        saveContact(argv.nama, argv.email, argv.noHp);
+    }
+});
 
-    contacts.saveContact(nama, email, noHp );
-
-}
-
-main();
-
+yargs.parse();
